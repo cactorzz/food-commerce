0c4c2a431cab243c1c7f8ace067165b0630dbda9
Hector Jimenez <euhectorjimenez@gmail.com>
2023-05-08 19:00:41 -0400

    Implementando a Sidebar
    Criando a parte visual da sidebar

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,3 +1,44 @@
+import { Container } from "./styles"
+
+import { ReactComponent as BurguerIcon } from '../../assets/burger.svg'
+import { ReactComponent as PizzaIcon } from '../../assets/pizza.svg'
+import { ReactComponent as SodaPopIcon } from '../../assets/soda.svg'
+import { ReactComponent as IceCreamIcon } from '../../assets/ice-cream.svg'
+
+import menuImg from '../../assets/menu.svg'
+
 export function Sidebar() {
-  return <h1>Sidebar</h1>
+  return <Container>
+    <button type="button">
+      <img src={menuImg} alt='Abrir e fechar o menu' />
+    </button>
+    <nav>
+      <ul>
+        <li>
+          <a href="#" className="active">
+            <BurguerIcon />
+            <span>Hambúrgueres</span>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <PizzaIcon />
+            <span>Pizzas</span>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <SodaPopIcon />
+            <span>Bebidas</span>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <IceCreamIcon />
+            <span>Sorvetes</span>
+          </a>
+        </li>
+      </ul>
+    </nav>
+  </Container>
 }
