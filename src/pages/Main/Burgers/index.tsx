29b62b25d9d9c91836e85d2686d4bf6725b5ef4f
Hector Jimenez <euhectorjimenez@gmail.com>
2023-05-13 13:39:36 -0400

    Carregando os dados via API
    Carregando os dados usando axios e json-server

--- src/pages/Main/Burgers/index.tsx
+++ src/pages/Main/Burgers/index.tsx
@@ -1,32 +1,27 @@
+import { useEffect, useState } from "react"
+
 import { Head } from "../../../components/Head"
 import { SnackTitle } from "../../../components/SnackTitle"
 import { Snacks } from "../../../components/Snacks"
 
+import { getBurgers } from "../../../services/api"
+
 export default function Burgers() {
-  const data = [
-    {
-      id: 1,
-      snack: 'burguer',
-      name: 'Mega',
-      description: 'O artesanal tamanho família rechado com três carnes suculentas, queijo e bacon.',
-      price: 25.5,
-      image: 'https://i.imgur.com/upjIUnG.jpg',
-    },
-    {
-      id: 2,
-      snack: 'burguer',
-      name: 'Extra bacon',
-      description: 'Criado para os amantes de bacon, possui em todas as suas camadas bacon bem assado e ainda queijo e carne.',
-      price: 23.5,
-      image: 'https://i.imgur.com/B4J04AJ.jpg',
-    },
-  ]
+  const [burgers, setBurgers] = useState([])
+
+  useEffect(() => {
+    ;(async () => {
+      const burgerRequest = await getBurgers()
+
+      setBurgers(burgerRequest.data)
+    })()
+  }, [])
 
   return (
   <>
     <Head title="Hambúrgueres" description="Nossos melhores burguers" />
     <SnackTitle>Hambúrgueres</SnackTitle>
-    <Snacks snacks={data}></Snacks>
+    <Snacks snacks={burgers}></Snacks>
   </>
   )
 }
