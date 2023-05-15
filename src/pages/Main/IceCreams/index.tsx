e5f21065a75a130dc0c80e080745edc7c47ff973
Hector Jimenez <euhectorjimenez@gmail.com>
2023-05-15 09:29:00 -0400

    Tipando o projeto
    Definindo a estrutura de tipos do projeto.

--- src/pages/Main/IceCreams/index.tsx
+++ src/pages/Main/IceCreams/index.tsx
@@ -5,9 +5,10 @@ import { SnackTitle } from "../../../components/SnackTitle"
 import { Snacks } from "../../../components/Snacks"
 
 import { getIceCreams } from "../../../services/api"
+import { SnackData } from "../../../interfaces/SnackData"
 
 export default function IceCreams() {
-  const [iceCreams, setIceCreams] = useState([])
+  const [iceCreams, setIceCreams] = useState<SnackData[]>([])
 
   useEffect(() => {
     ;(async () => {
