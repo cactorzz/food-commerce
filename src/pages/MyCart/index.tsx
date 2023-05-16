dbbe0caf0ab7d375869433e9360a08673de948ad
Hector Jimenez <euhectorjimenez@gmail.com>
2023-05-16 09:19:25 -0400

    Cart Page
    Criando a pagina Cart

---
+++ src/pages/MyCart/index.tsx
@@ -0,0 +1,11 @@
+import { Outlet } from 'react-router-dom'
+
+import { Container } from './styles'
+
+export default function Main() {
+  return (
+    <Container>
+      <h1>My Cart</h1>
+    </Container>
+  )
+}
