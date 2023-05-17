d53b5743eae220beaefc690d95071828874a5b71
Hector Jimenez <euhectorjimenez@gmail.com>
2023-05-17 09:18:37 -0400

    Valor total do cart
    Implementando o valor total do carrinho

--- src/contexts/CartContext.tsx
+++ src/contexts/CartContext.tsx
@@ -1,4 +1,4 @@
-import { ReactNode, createContext, useEffect, useState } from 'react'
+import { ReactNode, createContext, useState } from 'react'
 import { toast } from 'react-toastify'
 
 import { SnackData } from '../interfaces/SnackData'
