b5bfdb3b0fa924011d8eb85d8d646cc8a6961eaf
Hector Jimenez <euhectorjimenez@gmail.com>
2023-05-17 08:19:33 -0400

    Function remove snack
    Implementando a função de remover snack do cart

--- src/contexts/CartContext.tsx
+++ src/contexts/CartContext.tsx
@@ -10,22 +10,13 @@ interface Snack extends SnackData {
   subtotal: number
 }
 
-interface RemoveSnackFromCart {
-  id: number
-  snack: string
-}
-
-interface UpdateCartProps {
-  id: number
-  snack: string
-  newQuantity: number
-}
-
 interface CartContextProps {
   cart: Snack[]
   addSnackIntoCart: (snack: SnackData) => void
-  //removeSnackFromCart: ({id, snack}: RemoveSnackFromCart) => void
-  //updateCart: ({id, snack, newQuantity}: UpdateCartProps) => void
+  removeSnackFromCart: (snack: Snack) => void
+  snackCartIncrement: (snack: Snack) => void
+  snackCartDecrement: (snack: Snack) => void
+  confirmOrder: () => void
 }
 
 interface CartProviderProps {
@@ -69,8 +60,39 @@ export function CartProvider({ children }: CartProviderProps) {
     setCart(newCart)
   }
 
+  function removeSnackFromCart(snack: Snack) {
+    const newCart = cart.filter((item) => !(item.id === snack.id && item.snack === snack.snack))
+
+    setCart(newCart)
+  }
+
+  function updateSnackQuantity(snack: Snack, newQuantity: number) {
+    return
+  }
+
+  function snackCartIncrement(snack: Snack) {
+    updateSnackQuantity(snack, snack.quantity + 1)
+  }
+
+  function snackCartDecrement (snack: Snack) {
+    updateSnackQuantity(snack, snack.quantity - 1)
+  }
+
+  function confirmOrder() {
+    return
+  }
+
   return (
-    <CartContext.Provider value={{cart, addSnackIntoCart}}>
+    <CartContext.Provider
+      value={{
+        cart,
+        addSnackIntoCart,
+        removeSnackFromCart,
+        snackCartIncrement,
+        snackCartDecrement,
+        confirmOrder,
+      }}
+    >
       {children}
     </CartContext.Provider>
   )
--- src/pages/MyCart/Table/TableDesktop/index.tsx
+++ src/pages/MyCart/Table/TableDesktop/index.tsx
@@ -1,4 +1,6 @@
 import { currencyFormat } from "../../../../helpers/currencyFormat"
+
+import { FaTrashAlt } from 'react-icons/fa'
 import { useCart } from "../../../../hooks/useCart"
 
 import  plusImg  from '../../../../assets/circle-plus.svg'
@@ -7,7 +9,7 @@ import  minusImg  from '../../../../assets/circle-minus.svg'
 import { Container } from "./styles"
 
 export function TableDesktop() {
-  const {cart} = useCart()
+  const { cart, removeSnackFromCart } = useCart()
 
   return (
     <Container>
@@ -46,7 +48,11 @@ export function TableDesktop() {
                 <td>
                   <h5>{currencyFormat(item.subtotal)}</h5>
                 </td>
-                <td>Deletar</td>
+                <td>
+                  <button type="button" onClick={() => removeSnackFromCart(item)}>
+                   <FaTrashAlt />
+                  </button>
+                </td>
               </tr>
             ))
           }
