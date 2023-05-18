3b7aad78e1f9866272e4b46f5c5c9c59d359b433
Hector Jimenez <euhectorjimenez@gmail.com>
2023-05-18 10:43:07 -0400

    Primeira validação
    Criando a primeira validação usando a biblioteca React Hook Form.

--- src/pages/Payment/index.tsx
+++ src/pages/Payment/index.tsx
@@ -1,21 +1,37 @@
+import { useForm, SubmitHandler } from "react-hook-form";
+
 import { Head } from "../../components/Head";
 import { PayOrder } from "../../components/OrderCloseAction/PayOrder/index.tsx";
 import { OrderHeader } from "../../components/OrderHeader";
 
 import { Container, Inner, Form } from "./styles";
 
+type FieldValues = {
+  fullName: string
+  email: string
+  mobile: string
+}
+
 export default function Payment() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FieldValues>()
+  const onSubmit: SubmitHandler<FieldValues> = (data) => console.log(data)
+
   return (
     <Container>
       <Head title='Pagamento' />
       <OrderHeader />
       <Inner>
-        <Form>
+        <Form onSubmit={handleSubmit(onSubmit)}>
           <h4>Informações pessoais</h4>
 
           <div className="field">
-            <label htmlFor="full-name">Nome e sobrenome</label>
-            <input type="text" id="full-name" name="full-name" autoComplete="name" />
+            <label htmlFor="fullName">Nome e sobrenome</label>
+            <input type="text" id="fullName" autoComplete="name" {...register('fullName', { required: true })} />
+            {errors.fullName && <p className="error">O nome e sobrenome é um campo obrigatório.</p>}
           </div>
 
           <div className="grouped">
@@ -156,8 +172,8 @@ export default function Payment() {
               />
             </div>
           </div>
+          <PayOrder />
         </Form>
-        <PayOrder />
       </Inner>
     </Container>
   )
